1ba3f2eb62ef2eb692d06a4d89ddc6ce76dd0ba6
Anton <antonstrick@gmail.com>
2021-07-14 22:04:45 -0400

    Initial ElectroMagnet Unit Tests

---
+++ test/RobotTests/RobotController_Tests.cpp
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+#include <array>
+#include <unity.h>
+
+#include "config.hpp"
+#include "RobotControl.hpp"
+
+void test_Enable_Magnet(void) {
+    RobotControl subject;
+    subject.enableMagnet();
+    TEST_ASSERT_EQUAL(HIGH, digitalRead(MAGNET_PIN));
+}
+
+void test_Disable_Magnet(void) {
+    RobotControl subject;
+    subject.disableMagnet();
+    TEST_ASSERT_EQUAL(LOW, digitalRead(MAGNET_PIN));
+}
+
+void setup() {
+    configPins(); // located in config
+    UNITY_BEGIN();
+}
+
+void loop() {
+    static uint8_t loops = 0;
+
+    RUN_TEST(test_Enable_Magnet);
+    delay(1500);
+
+    RUN_TEST(test_Disable_Magnet);    
+    delay(1500);
+
+    if (loops < 3) {
+        UNITY_END();
+    }
+
+    loops++;
+}
