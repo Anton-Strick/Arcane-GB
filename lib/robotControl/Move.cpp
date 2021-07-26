9eee03caee9ab2c8d2e39d43576c57b3d117a517
Anton-Strick <antonstrick@gmail.com>
2021-07-25 20:23:40 -0400

    Revert "Corrected transposition equation"
    This reverts commit 1cfdf01162c0a0608dcaad7682151d5800b1a8b5.

--- lib/robotControl/Move.cpp
+++ lib/robotControl/Move.cpp
@@ -98,5 +98,5 @@ Move& Move::operator= (const Move& param) {
 
 void Move::halfSteps() {
     for (int i ; i < NUM_MOTORS ; i++)
-        numSteps[i] = uint32_t((numSteps[i] / 2) * 1.4142);
+        numSteps[i] = numSteps[i] / 2;
 }
